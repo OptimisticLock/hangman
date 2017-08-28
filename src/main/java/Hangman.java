9cc8468fa6ac56de616f814a4d496c6f46bc3fd8
Irina <izAppleApex@optimisticlock.com>
2017-08-28 13:58:30 -0700

    refactored toJson()

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -8,6 +8,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -23,24 +24,27 @@ public class Hangman {
 
     DefaultHttpClient httpClient = new DefaultHttpClient();// TODO:deprecated; TODO:manage reconnects
     HttpHost host = new HttpHost("hangman-api.herokuapp.com", 80, "http");
+    private String[] words;
 
     public static void main(String[] args) {
         new Hangman().run();
     }
 
 
-    public void readLines() throws FileNotFoundException { // TODO exception
+    public String[] readWords() throws FileNotFoundException { // TODO exception
 
         Scanner sc = new Scanner(new File("words"));
         List<String> lines = new ArrayList<String>();
-        while (sc.hasNextLine()) {
+
+        while (sc.hasNextLine())
             lines.add(sc.nextLine());
-        }
 
-        String[] arr = lines.toArray(new String[0]);
+        String[] result = lines.toArray(new String[0]);
+        return result;
 
-    //    FileUtils.readLines(new File("/path/filename"));
+        //    FileUtils.readLines(new File("/path/filename"));
     }
+
     /**
      * @return Game token   // curl hangman-api.herokuapp.com/hangman -X POST
      * @throws Exception
@@ -52,52 +56,38 @@ public class Hangman {
         System.out.println("executing request to " + host);
 
         HttpResponse httpResponse = httpClient.execute(host, postRequest);
-        HttpEntity entity = httpResponse.getEntity();
+        JSONObject json = toJson(httpResponse);
 
-        System.out.println("----------------------------------------");
-        System.out.println(httpResponse.getStatusLine());
-        Header[] headers = httpResponse.getAllHeaders();
-        for (int i = 0; i < headers.length; i++) {
-            System.out.println(headers[i]);
-        }
-        System.out.println("----------------------------------------");
+        String token = (String) json.get("token");
+        return token;
 
+    }
 
-        if (entity != null) {
+    JSONObject toJson(HttpResponse response) throws IOException, JSONException {
+        HttpEntity entity = response.getEntity();
 
-            String retSrc = EntityUtils.toString(entity);
-            System.out.println("Entity:" + retSrc);
-            JSONObject result = new JSONObject(retSrc); //Convert String to JSON Object
-            String gameId = (String) result.get("token");
-            return gameId;
-        }
-        return null; // TODO
+        if (entity == null)
+            return null; // TODO
+
+        String retSrc = EntityUtils.toString(entity);
+        System.out.println("Entity:" + retSrc);
+        JSONObject result = new JSONObject(retSrc);
+        return result;
     }
 
     // Good:curl -X PUT -d token=eyJzb2x1dGlvbiI6InNwaXJvY2hldGljIiwiY29ycmVjdF9ndWVzc2VzIjpbImUiLCJyIl0sIndyb25nX2d1ZXNzZXMiOltdfQ== -d letter=o http://hangman-api.herokuapp.com/hangman
     // Bad: curl -X PUT -d token=eyJzb2x1dGlvbiI6InJoeXRobWljIiwiY29ycmVjdF9ndWVzc2VzIjpbXScyI6W119 -d letter=a http://hangman-api.herokuapp.com/hangman
 
-    private void guessNextMove(String gameId) throws IOException {
+    private String guessNextMove(String gameId, char letter) throws IOException, JSONException {
         // PUT /hangman { token: game token, letter: guess }
 
-        String data = "{ token: '" + gameId + "', letter: 'a'}";
+        //     String data = "{ token: '" + gameId + "', letter: letter}";
         HttpPut request = new HttpPut("/hangman");
 
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("token", gameId));
-        params.add(new BasicNameValuePair("letter", "a"));
+        params.add(new BasicNameValuePair("letter", "" + letter));
         request.setEntity(new UrlEncodedFormEntity(params));
-   //     request.setHeader("Content-type", "multipart/form-data");
-
-        // TODO are these necessary?Review them
-    //    StringEntity params = new StringEntity(data, "UTF-8");
-    //    params.setContentType("application/json");
-     //   request.addHeader("content-type", "application/json");
-    //    request.addHeader("Accept", "*/*");
-//        request.addHeader("Accept-Encoding", "gzip,deflate,sdch");
-//        request.addHeader("Accept-Language", "en-US,en;q=0.8");
-     //   request.setEntity(params);
-
         System.out.println("executing request to " + request);
 
         HttpResponse httpResponse = httpClient.execute(host, request);
@@ -105,23 +95,32 @@ public class Hangman {
 
         System.out.println("----------------------------------------");
         System.out.println(httpResponse.getStatusLine());
-        Header[] headers = httpResponse.getAllHeaders();
-        for (int i = 0; i < headers.length; i++) {
-            System.out.println(headers[i]);
-        }
-        System.out.println("----------------------------------------");
+        //    Header[] headers = httpResponse.getAllHeaders();
+        //    for (int i = 0; i < headers.length; i++)
+        //        System.out.println(headers[i]);
+        //        System.out.println("----------------------------------------");
 
-        if (entity != null) {
-            System.out.println(EntityUtils.toString(entity));
-        }
+      //  {"hangman":"____a_","correct":true,"token":"eyJzb2x1dGlvbiI6ImNlbnRhbCIsImNvcnJlY3RfZ3Vlc3NlcyI6WyJhIl0sIndyb25nX2d1ZXNzZXMiOltdfQ=="}
+        JSONObject json = toJson(httpResponse);
+        String token = (String) json.get("token");
+        return token;
     }
 
+    char lastLetter = 'a';
+
+    private char guess() {
+        return lastLetter++; // TODO assert that <= 'z', also check for case sensitivity
+    }
 
     private void run() {
         try {
-            readLines();
+            words = readWords();
             String gameId = newGame();
-            guessNextMove(gameId);
+
+            while (true) {
+                char letter = guess();
+                guessNextMove(gameId, letter);
+            }
 
         } catch (Exception e) { // TODO be specific
             e.printStackTrace();
@@ -132,7 +131,5 @@ public class Hangman {
             httpClient.getConnectionManager().shutdown();
         }
     }
-
-
 }
 
