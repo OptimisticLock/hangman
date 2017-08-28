999d2e85474ba14f327514c38fa79ec28b02ebc2
Irina <izAppleApex@optimisticlock.com>
2017-08-28 13:22:33 -0700

    Bilal signed off

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,16 +1,18 @@
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
+import com.sun.org.glassfish.gmbal.NameValue;
+import org.apache.http.*;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 // https://alvinalexander.com/java/java-apache-httpclient-restful-client-examples
 
@@ -25,7 +27,7 @@ public class Hangman {
 
 
     /**
-     * @return Game token
+     * @return Game token   // curl hangman-api.herokuapp.com/hangman -X POST
      * @throws Exception
      */
     public String newGame() throws Exception {  // TODO
@@ -57,20 +59,29 @@ public class Hangman {
         return null; // TODO
     }
 
+    // Good:curl -X PUT -d token=eyJzb2x1dGlvbiI6InNwaXJvY2hldGljIiwiY29ycmVjdF9ndWVzc2VzIjpbImUiLCJyIl0sIndyb25nX2d1ZXNzZXMiOltdfQ== -d letter=o http://hangman-api.herokuapp.com/hangman
+    // Bad: curl -X PUT -d token=eyJzb2x1dGlvbiI6InJoeXRobWljIiwiY29ycmVjdF9ndWVzc2VzIjpbXScyI6W119 -d letter=a http://hangman-api.herokuapp.com/hangman
+
     private void guessNextMove(String gameId) throws IOException {
         // PUT /hangman { token: game token, letter: guess }
 
         String data = "{ token: '" + gameId + "', letter: 'a'}";
         HttpPut request = new HttpPut("/hangman");
 
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("token", gameId));
+        params.add(new BasicNameValuePair("letter", "a"));
+        request.setEntity(new UrlEncodedFormEntity(params));
+   //     request.setHeader("Content-type", "multipart/form-data");
+
         // TODO are these necessary?Review them
-        StringEntity params = new StringEntity(data, "UTF-8");
-        params.setContentType("application/json");
-        request.addHeader("content-type", "application/json");
-        request.addHeader("Accept", "*/*");
+    //    StringEntity params = new StringEntity(data, "UTF-8");
+    //    params.setContentType("application/json");
+     //   request.addHeader("content-type", "application/json");
+    //    request.addHeader("Accept", "*/*");
 //        request.addHeader("Accept-Encoding", "gzip,deflate,sdch");
 //        request.addHeader("Accept-Language", "en-US,en;q=0.8");
-        request.setEntity(params);
+     //   request.setEntity(params);
 
         System.out.println("executing request to " + request);
 
