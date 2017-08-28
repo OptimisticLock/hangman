877491248e7edadd555aebf413dcdaa7667ad5b2
Irina <izAppleApex@optimisticlock.com>
2017-08-28 12:19:56 -0700

    JSONObject compiles

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -2,28 +2,84 @@ import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 // https://alvinalexander.com/java/java-apache-httpclient-restful-client-examples
 
 public class Hangman {
 
-    DefaultHttpClient client;
-    HttpResponse asdfadf;
     DefaultHttpClient httpClient = new DefaultHttpClient();// TODO:deprecated; TODO:manage reconnects
-    HttpHost target = new HttpHost("hangman-api.herokuapp.com", 80, "http");
+    HttpHost request = new HttpHost("hangman-api.herokuapp.com", 80, "http");
+
+    public static void main(String[] args) {
+        new Hangman().run();
+    }
 
 
-    public void newGame() throws Exception {  // TODO
+    /**
+     * @return Game token
+     * @throws Exception
+     */
+    public String newGame() throws Exception {  // TODO
         // specify the get request
         HttpPost postRequest = new HttpPost("/hangman");
 
-        System.out.println("executing request to " + target);
+        System.out.println("executing request to " + request);
 
-        HttpResponse httpResponse = httpClient.execute(target, postRequest);
+        HttpResponse httpResponse = httpClient.execute(request, postRequest);
+        HttpEntity entity = httpResponse.getEntity();
+
+        System.out.println("----------------------------------------");
+        System.out.println(httpResponse.getStatusLine());
+        Header[] headers = httpResponse.getAllHeaders();
+        for (int i = 0; i < headers.length; i++) {
+            System.out.println(headers[i]);
+        }
+        System.out.println("----------------------------------------");
+
+
+        if (entity != null) {
+            System.out.println(EntityUtils.toString(entity));
+
+
+            String retSrc = EntityUtils.toString(entity);
+            // parsing JSON
+
+            JSONObject result = new JSONObject(retSrc); //Convert String to JSON Object
+
+            JSONArray tokenList = result.getJSONArray("names");
+            JSONObject oj = tokenList.getJSONObject(0);
+            String token = oj.getString("name");
+
+        }
+        return "Hello!";
+    }
+
+    private void guess() throws IOException {
+        // PUT /hangman { token: game token, letter: guess }
+
+        String data = "{ token: 'game token', letter: 'a'}";
+        HttpPut request = new HttpPut("/hangman");
+
+        StringEntity params = new StringEntity(data, "UTF-8");
+        params.setContentType("application/json");
+        request.addHeader("content-type", "application/json");
+        request.addHeader("Accept", "*/*");
+        request.addHeader("Accept-Encoding", "gzip,deflate,sdch");
+        request.addHeader("Accept-Language", "en-US,en;q=0.8");
+        request.setEntity(params);
+
+        System.out.println("executing request to " + request);
+
+        HttpResponse httpResponse = httpClient.execute(request);
         HttpEntity entity = httpResponse.getEntity();
 
         System.out.println("----------------------------------------");
@@ -37,16 +93,13 @@ public class Hangman {
         if (entity != null) {
             System.out.println(EntityUtils.toString(entity));
         }
-
     }
 
-    public static void main(String[] args) {
-        new Hangman().run();
-    }
 
     private void run() {
         try {
-            newGame();
+            String token = newGame();
+            guess();
 
         } catch (Exception e) { // TODO be specific
             e.printStackTrace();
@@ -57,5 +110,7 @@ public class Hangman {
             httpClient.getConnectionManager().shutdown();
         }
     }
+
+
 }
 
